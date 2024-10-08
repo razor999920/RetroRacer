c1724fb9aac3a2dc94a254f1f86cb57546f7cf06
razor999920 <raza9999ali@gmail.com>
2024-10-07 19:58:28 -0600

    first implementation of the update function

--- cmd/main.go
+++ cmd/main.go
@@ -3,35 +3,55 @@ package main
 import (
 	_ "image/png"
 	"log"
-	"math"
 	assets "terminalRacer"
 
 	"github.com/hajimehoshi/ebiten/v2"
 )
 
-var CarSprite *ebiten.Image
+var PlayerSprite *ebiten.Image
 
 func init() {
 	var err error
 
-	CarSprite, err = assets.LoadImage("ui/static/gameModels/car.png")
+	PlayerSprite, err = assets.LoadImage("ui/static/gameModels/car.png")
 	if err != nil {
 		log.Fatalf("Failed to load car sprite: %v", err)
 	}
 }
 
-type Game struct{}
+type Vector struct {
+	X float64
+	Y float64
+}
+type Game struct {
+	playerPosition Vector
+}
 
 func (g *Game) Update() error {
+	speed := float64(300 / ebiten.TPS())
+
+	if ebiten.IsKeyPressed(ebiten.KeyDown) {
+		g.playerPosition.Y += speed
+	}
+	if ebiten.IsKeyPressed(ebiten.KeyUp) {
+		g.playerPosition.Y -= speed
+	}
+	if ebiten.IsKeyPressed(ebiten.KeyLeft) {
+		g.playerPosition.X -= speed
+	}
+	if ebiten.IsKeyPressed(ebiten.KeyRight) {
+		g.playerPosition.X += speed
+	}
+
 	return nil
 }
 
 func (g *Game) Draw(screen *ebiten.Image) {
 	options := &ebiten.DrawImageOptions{}
-	options.GeoM.Rotate(90.0 * math.Pi / 180.0)
-	options.GeoM.Translate(150, 100)
+	// options.GeoM.Rotate(90.0 * math.Pi / 180.0)
+	options.GeoM.Translate(g.playerPosition.X, g.playerPosition.Y)
 
-	screen.DrawImage(CarSprite, options)
+	screen.DrawImage(PlayerSprite, options)
 }
 
 func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
@@ -39,7 +59,9 @@ func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeigh
 }
 
 func main() {
-	game := &Game{}
+	game := &Game{
+		playerPosition: Vector{X: 100, Y: 100},
+	}
 
 	if err := ebiten.RunGame(game); err != nil {
 		log.Fatal(err)
